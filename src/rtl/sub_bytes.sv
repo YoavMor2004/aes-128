9dd0565101751a219e3015bfceeb7558c2ec4f0e
Yoav Mor <yoavdor9@gmail.com>
2025-02-13 23:43:42 +0200

    Declared sub_bytes module

---
+++ src/rtl/sub_bytes.sv
@@ -0,0 +1,8 @@
+module sub_bytes (
+    input  logic [127:0] in_bus,   // 128-bit input bus
+    output logic [127:0] out_bus   // 128-bit output bus
+);
+
+    // Module logic goes here
+
+endmodule
