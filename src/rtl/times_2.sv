6f816764e495fbfce3c628cea408f5c59120c9ea
Yoav Mor <yoavdor9@gmail.com>
2025-02-15 20:30:37 +0200

    Fixed potential bug regarding the inconsistent endianness in the NIST documentation

--- src/rtl/times_2.sv
+++ src/rtl/times_2.sv
@@ -3,6 +3,6 @@ module times_2 (
     output logic [7:0] x_times_2   // byte output bus
 );
 
-    assign x_times_2 = (x[7] == 0) ? (x << 1) : ((x << 1) ^ 8'b11011000)
+    assign x_times_2 = (x[7] == 0) ? (x << 1) : ((x << 1) ^ 8'b00011011) // try with 8'hb1 as well
 
 endmodule
