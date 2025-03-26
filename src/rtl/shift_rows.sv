5c5cb45805b7d6f01ff4656b2a9b28492d559239
Yoav Mor <yoavdor9@gmail.com>
2025-03-26 17:14:29 +0000

    Fixed embarresing bug in shift_rows.sv

--- src/rtl/shift_rows.sv
+++ src/rtl/shift_rows.sv
@@ -29,6 +29,22 @@ module shift_rows (
     assign in_matrix[3][3] = in_bus[127:120];
 
 
+    assign out_matrix[1][0] = in_matrix[1][1];
+    assign out_matrix[1][1] = in_matrix[1][2];
+    assign out_matrix[1][2] = in_matrix[1][3];
+    assign out_matrix[1][3] = in_matrix[1][0];
+
+    assign out_matrix[2][0] = in_matrix[1][2];
+    assign out_matrix[2][1] = in_matrix[1][3];
+    assign out_matrix[2][2] = in_matrix[1][0];
+    assign out_matrix[2][3] = in_matrix[1][1];
+
+    assign out_matrix[3][0] = in_matrix[1][3];
+    assign out_matrix[3][1] = in_matrix[1][0];
+    assign out_matrix[3][2] = in_matrix[1][1];
+    assign out_matrix[3][3] = in_matrix[1][2];
+
+
     assign out_bus[7:0]     = out_matrix[0][0];
     assign out_bus[15:8]    = out_matrix[1][0];
     assign out_bus[23:16]   = out_matrix[2][0];
@@ -48,21 +64,5 @@ module shift_rows (
     assign out_bus[111:104] = out_matrix[1][3];
     assign out_bus[119:112] = out_matrix[2][3];
     assign out_bus[127:120] = out_matrix[3][3];
-
-
-    assign out_matrix[1][0] = in_matrix[1][1];
-    assign out_matrix[1][1] = in_matrix[1][2];
-    assign out_matrix[1][2] = in_matrix[1][3];
-    assign out_matrix[1][3] = in_matrix[1][0];
-
-    assign out_matrix[2][0] = in_matrix[1][2];
-    assign out_matrix[2][1] = in_matrix[1][3];
-    assign out_matrix[2][2] = in_matrix[1][0];
-    assign out_matrix[2][3] = in_matrix[1][1];
-
-    assign out_matrix[3][0] = in_matrix[1][3];
-    assign out_matrix[3][1] = in_matrix[1][0];
-    assign out_matrix[3][2] = in_matrix[1][1];
-    assign out_matrix[3][3] = in_matrix[1][2];
 
  endmodule
