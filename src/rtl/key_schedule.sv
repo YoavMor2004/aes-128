d1230139c216b9108b7d97c473098842bbe6bdba
Yoav Mor <yoavdor9@gmail.com>
2025-03-26 19:03:22 +0000

    Attempted fix of the key_schedule module

--- src/rtl/key_schedule.sv
+++ src/rtl/key_schedule.sv
@@ -9,12 +9,12 @@ module key_schedule (
     logic [31:0] in_rotated;
     logic [31:0] in_subbed;
 
-    rot_word rw(.in_bus(in_bus[127:96]), .out_bus(in_rotated));
-    sub_word sw(.in_bus(in_rotated),      .out_bus(in_subbed));
+    rot_word rw(.in_bus(in_bus[31:0]), .out_bus(in_rotated));
+    sub_word sw(.in_bus(in_rotated),   .out_bus(in_subbed));
 
-    assign out_bus[31:0]   = in_bus[31:0]   ^ rcon ^ in_subbed;
-    assign out_bus[63:32]  = in_bus[63:32]  ^ out_bus[31:0];
-    assign out_bus[95:64]  = in_bus[95:64]  ^ out_bus[63:32];
-    assign out_bus[127:96] = in_bus[127:96] ^ out_bus[95:64];
+    assign out_bus[127:96] = in_bus[127:96] ^ rcon ^ in_subbed;
+    assign out_bus[95:64]  = in_bus[95:64]  ^ out_bus[127:96];
+    assign out_bus[63:32]  = in_bus[63:32]  ^ out_bus[95:64];
+    assign out_bus[31:0]   = in_bus[31:0]   ^ out_bus[63:32];
 
 endmodule
