8fba77d579676c92d00eda221e092f4144fda87c
Yoav Mor <yoavdor9@gmail.com>
2025-09-06 19:04:53 +0000

    wtf was that * in the .git/info/ignore???

---
+++ scripts/run_multicycle_glv.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xrun $1 -sv -f ../scripts/libraries_src_list.f ../export/post_synth/aes_128.v ../src/tb/multicycle/aes_128_tb.sv -access +rw-c -INPUT ../scripts/simulation.tcl -neverwarn
