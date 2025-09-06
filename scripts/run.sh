2eda63ef5b6a0f8ef12f273851d3f8c822f90a1d
Yoav Mor <yoavdor9@gmail.com>
2025-09-06 17:32:21 +0300

    Reorganized File Structure to allow for additional designs

--- scripts/run.sh
+++
@@ -1,3 +0,0 @@
-# xrun -debug -nospecify -timescale 1ns/1ps -gui -access +rw-c -f ../src/src_list_aes_128.txt -sv -INPUT ../scripts/simulation.tcl -neverwarn
-
-xrun -gui -sv -f ../src/src_list_aes_128.txt -access +rw-c -INPUT ../scripts/simulation.tcl
---
+++ src/sim_combinational_aes_128.f
@@ -0,0 +1,3 @@
+-f ../src/dut_combinational_aes_128.txt
+
+../src/tb/combinational/aes_128_tb.sv
