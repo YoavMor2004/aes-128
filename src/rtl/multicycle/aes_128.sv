6f4c6500619ad4e1046bf7bedbfbfab78ae93b78
Yoav Mor <yoavdor9@gmail.com>
2025-09-06 19:44:51 +0300

    Implemented multicyce aes-128

---
+++ src/rtl/multicycle/aes_128.sv
@@ -0,0 +1,67 @@
+`timescale 1ns/1ps
+
+module aes_128 (
+    input logic clk,
+    input logic rst_n,
+
+    input  logic [127:0] in_bus,   // 128-bit input bus
+    input  logic [127:0] key,      // 128-bit input key
+    output logic [127:0] out_bus,  // 128-bit output bus
+
+    output logic valid_ready       // If valid - out_bus is encryption of previous in_bus with key
+                                   // If ready - accepts new inputs
+);
+    logic input_reg_n;
+    logic index [3:0];
+
+    logic xor_output_sb_input [127:0];
+    logic sb_output_sr_input  [127:0];
+    logic sr_output_mc_input  [127:0];
+    logic mc_output           [127:0];
+
+    logic rcon_msB [7:0];
+    logic rcon     [31:0];
+
+    controller c(.clk(clk), .rst_n(rst_n), .index(index), .input_reg_n(input_reg_n), .valid_ready(valid_ready));
+
+    logic [127:0] state;
+    logic [127:0] next_state;
+
+    logic [127:0] key_reg;
+    logic [127:0] next_key;
+
+    always @(posedge clk)
+        state   <= next_state;
+    always @(posedge clk)
+        key_reg <= next_key;
+
+    always @* begin
+        case (index)
+            4'd0:    rcon_msB = 8'h01;
+            4'd1:    rcon_msB = 8'h02;
+            4'd2:    rcon_msB = 8'h04;
+            4'd3:    rcon_msB = 8'h08;
+            4'd4:    rcon_msB = 8'h10;
+            4'd5:    rcon_msB = 8'h20;
+            4'd6:    rcon_msB = 8'h40;
+            4'd7:    rcon_msB = 8'h80;
+            4'd8:    rcon_msB = 8'h1b;
+            4'd9:    rcon_msB = 8'h36;
+            default: rcon_msB = 8'hxx;
+        endcase
+    end
+
+    assign rcon = {rcon_msB, 8'h00, 8'h00, 8'h00};
+    
+    assign k = input_reg_n ? key : key_reg;
+
+    assign xor_output_sb_input = (input_reg_n ? in_bus : state) ^ k;
+    assign next_state          = mc_output;
+    assign out_bus             = sr_output_mc_input             ^ next_key;
+
+    key_schedule ks(.in_bus(k                  ), .rcon(rcon), .out_bus(next_key          ));
+    sub_bytes    sb(.in_bus(xor_output_sb_input),              .out_bus(sb_output_sr_input));
+    shift_rows   sr(.in_bus(sb_output_sr_input ),              .out_bus(sr_output_mc_input));
+    mix_columns  mc(.in_bus(sr_output_mc_input ),              .out_bus(mc_output         ));
+
+endmodule
---
+++ src/rtl/multicycle/controller.sv
@@ -0,0 +1,39 @@
+module aes_128 (
+    input logic clk,
+    input logic rst_n,
+
+    output logic index [3:0],
+    output logic input_reg_n,
+    output logic valid_ready       // If valid - out_bus is encryption of previous in_bus with previous key
+                                   // If ready - accepts new inputs
+);
+
+    logic next_index [3:0];
+
+    always @(posedge clk or negedge rst_n) begin
+        if (~rst_n)
+            index <= 4'd9; // Start encryption at the next clock cycle
+        else            
+            index <= next_index;
+    end
+
+    always @* begin
+        case (index)
+            4'd0:    next_index = 4'd1;
+            4'd1:    next_index = 4'd2;
+            4'd2:    next_index = 4'd3;
+            4'd3:    next_index = 4'd4;
+            4'd4:    next_index = 4'd5;
+            4'd5:    next_index = 4'd6;
+            4'd6:    next_index = 4'd7;
+            4'd7:    next_index = 4'd8;
+            4'd8:    next_index = 4'd9;
+            4'd9:    next_index = 4'd0;
+            default: next_index = 4'dx;
+        endcase
+    end
+
+    assign input_reg_n = (index == 0) ? 1 : 0;
+    assign valid_ready = (index == 9) ? 1 : 0;
+
+endmodule
